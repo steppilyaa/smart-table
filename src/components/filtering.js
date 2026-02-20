aa6c2ca08476d710e8d1443f97d3b3f1d92e272c
Ilya Stepanov <stepiliya@yandex.ru>
2026-02-20 13:31:41 +0300

    first test

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,33 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import {compare, createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const comparefn = createComparison(defaultRules)
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes).forEach((elementName)  => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName]).map(name => {
+                const option = document.createElement('option')
+                option.value = name
+                option.textContent = name
+                return option
+            })
+        ) 
+    })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear')
+        {
+            const actionPar = action.parentElement
+            const closestInput = actionPar.querySelector('input')
+            closestInput.value = '';
+            state.customer = ''
 
+
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+
+        return data.filter(row => comparefn(row,state));
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './fonts/ys-display/fonts.css'
 import './style.css'
 
 import {data as sourceData} from "./data/dataset_1.js";
-
+import { initPagination } from './components/pagination.js';
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
-
+import {initSorting} from "./components/sorting.js"
 import {initTable} from "./components/table.js";
+import { initFiltering } from './components/filtering.js';
+import { initSearching } from './components/searching.js';
 // @todo: подключение
 
 
@@ -19,9 +21,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1)
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,21 +39,39 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-
-
+    result = applyPagination(result,state,action)
+    result = applySorting(result,state,action)
+    result = applyFiltering(result,state,action)
+    result = applySearch(result,state,action)
     sampleTable.render(result)
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search','header','filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el,page,isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span')
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+)
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
 
-
+const applyFiltering = initFiltering(sampleTable.filter.elements, {searchBySeller: indexes.sellers})
+const applySearch = initSearching('search')
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
